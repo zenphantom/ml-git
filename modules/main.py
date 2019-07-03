2b6df18f3e82bc4c2817fd23fed3fac9fa23d60c
Gilmar Bezerra de Freitas <gilmar.freitas@virtus.ufcg.edu.br>
2019-07-03 17:19:06 -0300

    #47398 - Change script entry point

--- modules/main.py
+++
@@ -1,9 +0,0 @@
-"""
-© Copyright 2020 HP Development Company, L.P.
-SPDX-License-Identifier: GPL-2.0-only
-"""
-
-# -*- coding: utf-8 -*-
-
-def main():
-  print("TODO: aidiconar código do entry point da aplicação")
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'ml-git = modules.main:main'
+            'ml-git = src.__main__:main'
         ]
     }
 )
