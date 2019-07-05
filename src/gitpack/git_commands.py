db4ad8410b835f3c41c943257609101e78927ee6
Tales Ribeiro Morais Gurjão <tales.gurjao@virtus.ufcg.edu.br>
2019-07-05 11:05:16 -0300

    #17651 - Adding pre-commit hook

--- src/gitpack/git_commands.py
+++ src/gitpack/git_commands.py
@@ -7,20 +7,21 @@ import os
 from pathlib import Path
 
 import git
-from time import *
-from os import path
 from git import Repo
 
-from utils.constants import *
+from utils import constants
+
 
 def handle_git_operation(path, action):
     if action == "add":
         add_files(path)
 
+
 # CLONE
 def clone_repo(repo_path, repo_url):
     Repo.clone_from(repo_url,
                     repo_path)  # clone repository
+
 
 # ADD
 def add_files(file):
@@ -29,9 +30,10 @@ def add_files(file):
     index = repo.index
     # The index contains all blobs in a flat list
     # adding files
-    # new_file_path = os.path.join(DATA_SET_DEFAULT_DIR, file)
-    data_set_path = os.path.realpath(DATA_SET_DEFAULT_DIR if file is None else file)
+    # new_file_path = os.path.join(TRACKER_DEFAULT_DIR, file)
+    data_set_path = os.path.realpath(constants.TRACKER_DEFAULT_DIR if file is None else file)
     index.add([data_set_path])  # add a new file to the index
+
 
 # COMMIT
 def commit_files(filename):
@@ -40,11 +42,12 @@ def commit_files(filename):
     # The index contains all blobs in a flat list
     index.commit("Adding " + filename + "to repo")
 
-    #TODO get correct useremail and commit message
+    # TODO get correct useremail and commit message
     author = git.Actor("An author", repo.useremail)
     committer = git.Actor("A committer", repo.useremail)
     # commit by commit message and author and committer
     index.commit("my commit message", author=author, committer=committer)
+
 
 #  ----------------------------------------
 
@@ -55,19 +58,20 @@ def push_files(repo_path):
     origin = repo.remote(name='origin')
     origin.push()
 
+
 # CHECKOUT
 def commit_checkout_files(repo_path):
     repo = git.Repo(repo_path)
 
     # TODO get correct branch name and commit message
-    if repo != None:
+    if repo is not None:
         new_branch = 'new_branch'
         current = repo.create_head(new_branch)
         current.checkout()
         master = repo.heads.master
         repo.git.pull('origin', master)
 
-        #TODO check commit of new branch
+        # TODO check commit of new branch
         if repo.index.diff(None) or repo.untracked_files:
 
             repo.git.add(A=True)
@@ -77,6 +81,7 @@ def commit_checkout_files(repo_path):
         else:
             print('no changes')
 
+
 # list
 def list_branches(repo_path):
     repo = git.Repo(repo_path)
@@ -85,12 +90,14 @@ def list_branches(repo_path):
     repo_heads_names = [h.name for h in repo_heads]
     print(repo_heads_names)
 
+
 def list_tags(repo_path):
     repo = git.Repo(repo_path)
 
     repo_tags = repo.tags
     tags = [h.tag for h in repo_tags]
     print(tags)
+
 
 def list_commits(repo_path):
     repo = git.Repo(repo_path)
@@ -99,11 +106,13 @@ def list_commits(repo_path):
     commits = [h.commit for h in repo_heads]
     print(commits)
 
+
 def diff_commits(repo_path):
     repo = git.Repo(repo_path)
     count_modified_files = len(repo.index.diff(None))
     count_staged_files = len(repo.index.diff("HEAD"))
     print(count_modified_files, count_staged_files)
+
 
 def _get_repository_root():
     current_path = Path(os.getcwd())
@@ -119,6 +128,3 @@ def _get_repository_root():
             else:
                 current_path = parent
     return None
-
-
-
