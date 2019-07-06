1916fc603315f97f205a7d7fed2f4606e9c6ec7f
Sebastien Tandel <sebastien.tandel@gmail.com>
2019-07-06 20:12:08 -0300

    adding Manifest and its unit tests

---
+++ src/mlgit/manifest.py
@@ -0,0 +1,79 @@
+"""
+© Copyright 2020 HP Development Company, L.P.
+SPDX-License-Identifier: GPL-2.0-only
+"""
+
+from mlgit.utils import yaml_load, yaml_save
+from pprint import pformat
+
+
+class Manifest(object):
+	def __init__(self, manifest):
+		self._mfpath = manifest
+		self._manifest = yaml_load(manifest)
+
+	def add(self, key, file, stats=None):
+		mf = self._manifest
+		try:
+			mf[key].append(file)
+		except:
+			mf[key] = [file]
+
+	def rm(self, key, file):
+		mf = self._manifest
+		try:
+			files = mf[key]
+			if len(files) == 1:
+				del(mf[key])
+			else:
+				files.remove(file)
+				mf[key] = files
+		except Exception as e:
+			print(e)
+			return False
+		return True
+
+	def rm_file(self, file):
+		mf = self._manifest
+		for key in mf:
+			files = mf[key]
+			if file not in files: continue
+
+			if len(files) == 1:
+				del(mf[key])
+			else:
+				files.remove(file)
+				mf[key] = files
+			return True
+		return False
+
+
+	def exists(self, key):
+		return key in self._manifest
+
+	def search(self, file):
+		mf = self._manifest
+		for key in mf:
+			if file in mf[key]: return key
+		return None
+
+	def get(self, key):
+		try:
+			return self._manifest[key]
+		except:
+			return None
+
+	def exists_keyfile(self, key, file):
+		mf = self._manifest
+		try:
+			files = mf[key]
+			return file in files
+		except:
+			pass
+		return False
+
+	def __repr__(self):
+		return pformat(self._manifest, indent=4)
+
+	def save(self):
+		yaml_save(self._manifest, self._mfpath)
---
+++ test/test_manifest.py
@@ -0,0 +1,123 @@
+"""
+© Copyright 2020 HP Development Company, L.P.
+SPDX-License-Identifier: GPL-2.0-only
+"""
+
+from mlgit.manifest import Manifest
+import unittest
+import tempfile
+import os
+
+class ManifestTestCases(unittest.TestCase):
+	def test_add(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			self.assertTrue(mf.exists("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u"))
+
+	def test_add2(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+	def test_search(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			self.assertEqual(mf.search("data/think-hires.jpg"), "zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u")
+			self.assertEqual(mf.search("data/think-hires2.jpg"), "zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u")
+
+	def test_save(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			mf.save()
+
+			mf = Manifest(mfpath)
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+	def test_rm(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+			mf.rm("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			self.assertTrue(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertFalse(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+	def test_rmfile(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+			self.assertTrue(mf.rm_file("data/think-hires2.jpg"))
+			self.assertTrue(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertFalse(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+	def test_rm_allfiles(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+			self.assertTrue(mf.rm_file("data/think-hires2.jpg"))
+			self.assertTrue(mf.rm_file("data/think-hires.jpg"))
+
+			self.assertFalse(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertFalse(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+			self.assertFalse(mf.exists("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u"))
+
+	def test_rm_allfiles2(self):
+		with tempfile.TemporaryDirectory() as tmpdir:
+			mfpath = os.path.join(tmpdir, "manifest.yaml")
+
+			mf = Manifest(mfpath)
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg")
+			mf.add("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg")
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertTrue(mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+
+			self.assertTrue(mf.rm("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+			self.assertTrue(mf.rm("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+
+			self.assertFalse(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires.jpg"))
+			self.assertFalse(
+				mf.exists_keyfile("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u", "data/think-hires2.jpg"))
+			self.assertFalse(mf.exists("zdj7WgHSKJkoJST5GWGgS53ARqV7oqMGYVvWzEWku3MBfnQ9u"))
+if __name__ == "__main__":
+	unittest.main()
