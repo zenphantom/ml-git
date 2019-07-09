ade3c54fdccdd4b2b5d203e42a89df1f0982197b
Gilmar Bezerra de Freitas <gilmar.freitas@virtus.ufcg.edu.br>
2019-07-09 11:02:38 -0300

    #47712 - Change message colors

--- src/operations/status.py
+++ src/operations/status.py
@@ -26,10 +26,10 @@ def handle_status_operation(files):
                 pass
         item_status.filter_path(filtered_files)
 
-    _print_group('Files not tracked', 'Untracked', item_status.untracked, 'green')
-    _print_group('Files modified', 'Modified', item_status.modified, 'yellow')
-    _print_group('Files deleted', 'Deleted', item_status.deleted, 'red')
-    _print_group('Files tracked but not uploaded', 'Not Uploaded', item_status.not_uploaded, 'blue')
+    _print_group('Files not tracked', 'Untracked', item_status.untracked, 'red')
+    _print_group('Files modified', 'Modified', item_status.modified, 'green')
+    _print_group('Files deleted', 'Deleted', item_status.deleted, 'green')
+    _print_group('Files tracked but not uploaded', 'Not Uploaded', item_status.not_uploaded, 'green')
 
 
 def _print_group(message, title, items, color):
