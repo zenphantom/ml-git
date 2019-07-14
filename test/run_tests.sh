abf0823f80ee5db6a6d53d4a7b9acca25e095cb4
Sébastien Tandel <sebastien.tandel@hp.com>
2019-07-14 18:07:36 -0300

    script to run all unit tests with code coverage

---
+++ test/run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+################################################################################
+# © Copyright 2020 HP Development Company, L.P.
+# SPDX-License-Identifier: GPL-2.0-only
+################################################################################
+
+python -m nose --with-coverage --cover-package mlgit --cover-html-dir=coverage .
