c67b84a39f944788d2d983dc65b2116727664ebb
David Staas <david.staas@hp.com>
2019-07-24 11:46:57 -0700

    Make unit tests Windows-compatible

---
+++ test/run_tests.bat
@@ -0,0 +1,6 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: © Copyright 2020 HP Development Company, L.P.
+:: SPDX-License-Identifier: GPL-2.0-only
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+python -m nose --with-coverage --cover-package mlgit --cover-html-dir=coverage .
