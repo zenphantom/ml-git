811d946fce8922e6d8d2c90cc8b98a1303b44244
Sébastien Tandel <sebastien.tandel@hp.com>
2019-08-03 11:55:00 -0300

    thread pool with retry strategy & unit tests (100%)

--- src/mlgit/pool.py
+++ src/mlgit/pool.py
@@ -4,74 +4,60 @@ SPDX-License-Identifier: GPL-2.0-only
 """
 
 from mlgit import log
-import concurrent.futures
+from concurrent import futures
 from tqdm import tqdm
 
 class WorkerPool(object):
-	def __init__(self, nworkers=10, pool_ctxs=None, pb_elts=None, pb_desc="units"):
+	def __init__(self, nworkers=10, pool_ctxs=None, retry=0, pb_elts=None, pb_desc="units"):
 		if pool_ctxs is not None and len(pool_ctxs) != nworkers: return None
-		self._free_ctx = pool_ctxs
+		self._avail_ctx = pool_ctxs
 
-		self._pool = concurrent.futures.ThreadPoolExecutor(max_workers=nworkers)
-		self._progress_bar = tqdm(total=pb_elts, desc=pb_desc, unit=pb_desc, unit_scale=True, mininterval=1.0)
+		nwrkrs = nworkers if nworkers > 0 else 1
+		self._pool = futures.ThreadPoolExecutor(max_workers=nwrkrs)
+
 		self._futures = []
+		self._retry = retry if retry >= 0 else 0
 
-	def _submit_fn(self, userfn, *args):
-		try:
-			ctx = self._getctx() if self._free_ctx is not None else None
-			result = userfn(ctx, *args)
-			self._freectx(ctx) if ctx is not None else None
+		self._progress_bar = tqdm(total=pb_elts, desc=pb_desc, unit=pb_desc, unit_scale=True, mininterval=1.0) if pb_elts is not None else None
 
-			self._progress() if self._progress_bar is not None else None
-		except Exception as e:
-			log.error("WorkerPool: exception -- [%s]" % (e))
-			raise(e)
+	def _submit_fn(self, userfn, *args, **kwds):
+		ctx = self._get_ctx() if self._avail_ctx is not None else None
 
+		result = False
+		retry_cnt = 0
+		while True:
+			try:
+				if ctx is not None:
+					result = userfn(ctx, *args, **kwds)
+				else:
+					result = userfn(*args, **kwds)
+			except Exception as e:
+				if retry_cnt < self._retry:
+					retry_cnt += 1
+					log.warn("Pool: worker exception - [%s] -- retry [%d]" % (e, retry_cnt))
+					continue
+				else:
+					log.error("Pool: worker failure - [%s] -- [%d] attempts" % (e, retry_cnt))
+					raise(e)
+			break
+
+		log.debug("Pool: worker success at attempt [%d]" % (retry_cnt+1))
+		self._release_ctx(ctx) if ctx is not None else None
+		self._progress() if self._progress_bar is not None else None
 		return result
 
-	def submit(self, userfn, *args):
-		self._futures.append(self._pool.submit(self._submit_fn, userfn, *args))
+	def submit(self, userfn, *args, **kwds):
+		self._futures.append(self._pool.submit(self._submit_fn, userfn, *args, **kwds))
 
-	def _getctx(self):
-		return self._free_ctx.pop()
+	def _get_ctx(self):
+		return self._avail_ctx.pop()
 
-	def _freectx(self, ctx):
-		self._free_ctx.append(ctx)
+	def _release_ctx(self, ctx):
+		self._avail_ctx.append(ctx)
 
 	def _progress(self, units=1):
 		self._progress_bar.update(units)
 
 	def wait(self):
-		concurrent.futures.wait(self._futures)
-
-
-if __name__=="__main__":
-	class Context(object):
-		def __init__(self, i):
-			self._i = i
-
-		def __repr__(self):
-			return "context: %s" % str(self._i)
-
-	def fn(ctx, abc, bcd):
-		try:
-			import time
-			import random
-			# fail = random.randint(0, 1000)
-			# if fail > 700: raise Exception("bla bla bla")
-
-			# attempt to do something useful with my cpu cycles :)
-			time.sleep(float(random.randrange(1, 10)) / 100)
-			# print("%s -> %s" % (abc, ctx))
-		except Exception as e:
-			print(e)
-
-	nworkers = 10
-	njobs = nworkers*100
-	ctxs = [ Context(i) for i in range(nworkers) ]
-	wp = WorkerPool(nworkers, ctxs, njobs)
-
-	for i in range(njobs):
-		abc = "fn %d" % (i)
-		wp.submit(fn, abc, abc)
-	wp.wait()
+		futures.wait(self._futures)
+		return self._futures
