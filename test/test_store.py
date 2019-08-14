74a324a795206c617a5831e8184fa2e118831b93
Raiff Ramalho de Mello <raiff.mello@ccc.ufcg.edu.br>
2019-08-14 11:36:34 -0300

    #51149 - unit test changed to just test the push function

--- test/test_store.py
+++ test/test_store.py
@@ -20,23 +20,18 @@ import os
 from mlgit.utils import ensure_path_exists, yaml_save, yaml_load
 
 
-files_mock = {'zdj7Wm99FQsJ7a4udnx36ZQNTy7h4Pao3XmRSfjo4sAbt9g74': {'1.jpg'},
-			  'zdj7WnVtg7ZgwzNxwmmDatnEoM3vbuszr3xcVuBYrcFD6XzmW': {'2.jpg'},
-			  'zdj7Wi7qy2o3kgUC72q2aSqzXV8shrererADgd6NTP9NabpvB': {'3.jpg'},
-			  'zdj7We7FUbukkozcTtYgcsSnLWGqCm2PfkK53nwJWLHEtuef4': {'6.jpg'},
-			  'zdj7WZzR8Tw87Dx3dm76W5aehnT23GSbXbQ9qo73JgtwREGwB': {'7.jpg'},
-			  'zdj7WfQCZgACUxwmhVMBp4Z2x6zk7eCMUZfbRDrswQVUY1Fud': {'8.jpg'},
-			  'zdj7WdjnTVfz5AhTavcpsDT62WiQo4AeQy6s4UC1BSEZYx4NP': {'9.jpg'},
-			  'zdj7WXiB8QrNVQ2VABPvvfC3VW6wFRTWKvFhUW5QaDx6JMoma': {'10.jpg'}}
+files_mock = {'zdj7Wm99FQsJ7a4udnx36ZQNTy7h4Pao3XmRSfjo4sAbt9g74': {'1.jpg'}}
 
 testprofile = os.getenv('MLGIT_TEST_PROFILE', 'personal')
 testregion = os.getenv('MLGIT_TEST_REGION', 'us-east-1')
 testbucketname = os.getenv('MLGIT_TEST_BUCKET', 'ml-git-models')
+testbucketname_2 = os.getenv('MLGIT_TEST_BUCKET', 'ml-git-datasets')
 bucket = {
 	"aws-credentials": {"profile": testprofile},
 	"region": testregion
 }
 bucketname = testbucketname
+bucketname_2 = testbucketname_2
 
 def md5sum(file):
 	hash_md5 = hashlib.md5()
@@ -54,6 +49,7 @@ class S3StoreTestCases(unittest.TestCase):
 			aws_access_key_id="fake_access_key",
 			aws_secret_access_key="fake_secret_key",
 		)
+
 		try:
 			s3 = boto3.resource(
 				"s3",
@@ -68,6 +64,7 @@ class S3StoreTestCases(unittest.TestCase):
 			err = "{bucket} should not exist.".format(bucket=bucketname)
 			raise EnvironmentError(err)
 		client.create_bucket(Bucket=bucketname)
+		client.create_bucket(Bucket=bucketname_2)
 
 	def test_put(self):
 		s3store = S3MultihashStore(bucketname, bucket, blocksize=1024*1024)
@@ -89,7 +86,7 @@ class S3StoreTestCases(unittest.TestCase):
 			self.assertTrue(s3store.get(fpath, k))
 			self.assertEqual(md5sum(fpath), md5sum(f))
 
-	def test_get_with_push(self):
+	def test_push(self):
 		with tempfile.TemporaryDirectory() as tmpdir:
 
 			indexpath = os.path.join(tmpdir, "index-test")
@@ -103,8 +100,9 @@ class S3StoreTestCases(unittest.TestCase):
 			files = yaml_load(manifestpath)
 			# adds chunks to ml-git Index
 			idx = MultihashIndex(specpath, indexpath)
-			idx.add('data-test-push/', manifestpath)
+			idx.add('data-test-push-1/', manifestpath)
 			idx_hash = MultihashFS(indexpath)
+			print("index: ", idx_hash.get_log())
 			self.assertTrue(len(idx_hash.get_log()) > 0)
 			self.assertTrue(os.path.exists(indexpath))
 			c = yaml_load("hdata/config.yaml")
@@ -113,23 +111,14 @@ class S3StoreTestCases(unittest.TestCase):
 			spec = yaml_load(specpath + "/dataset-ex.spec")
 			manifest = spec['dataset']["manifest"]
 			store = store_factory(c, manifest["store"])
+			# s3store = S3MultihashStore(bucketname, bucket, blocksize=1024 * 1024)
 
 			self.assertTrue(os.path.exists(objectpath))
 
 			r = LocalRepository(c, objectpath)
 			r.push(indexpath, objectpath, specpath + "/dataset-ex.spec")
 			self.assertTrue(len(idx_hash.get_log()) == 0)
-			s3 = boto3.resource(
-				"s3",
-				region_name="eu-west-1",
-				aws_access_key_id="fake_access_key",
-				aws_secret_access_key="fake_secret_key",
-			)
 
-			s3.Bucket('ml-git-datasets')
-			for key in files:
-				keypath = r._keypath(key)
-				self.assertTrue(r._fetch_blob(key, keypath, store))
 
 
 	def tearDown(self):
