318c4cac36cfed0dfd49f4a9fb5599ba8475e9cf
Heitor <heitor.lira@virtus.ufcg.edu.br>
2019-08-15 13:09:49 -0300

    #50422 Add sample over the whole population

--- src/mlgit/main.py
+++ src/mlgit/main.py
@@ -16,15 +16,15 @@ from mlgit.schema_utils import main_validate
 def repository_entity_cmd(config, args):
 
 	repotype = "project"
-	if args["dataset"] == True:
+	if args["dataset"] is True:
 		repotype = "dataset"
-	if args["labels"] == True:
+	if args["labels"] is True:
 		repotype = "labels"
-	if args["model"] == True:
+	if args["model"] is True:
 		repotype = "model"
 
 	r = Repository(config, repotype)
-	if args["--verbose"] == True:
+	if args["--verbose"] is True:
 		print("ml-git config:")
 		pprint(config)
 		print("docopt argumens:")
@@ -46,25 +46,25 @@ def repository_entity_cmd(config, args):
 		if "--type" in args and args["--type"] is not None: type = args["--type"]
 		if "--region" in args and args["--region"] is not None: region = args["--region"]
 		if "--credentials" in args and args["--credentials"] is not None: credentials = args["--credentials"]
-		if args["store"] == True and args["add"] == True:
+		if args["store"] is True and args["add"] is True:
 			print("add store %s %s %s %s" % (type, bucket, credentials, region))
 			store_add(type, bucket, credentials, region)
 		return
 
 	remote_url = args["<ml-git-remote-url>"]
-	if args["remote"] == True and args["add"] == True:
+	if args["remote"] is True and args["add"] is True:
 		r.repo_remote_add(repotype, remote_url)
 		return
 
 	spec = args["<ml-entity-name>"]
 	retry = 2
 	if "--retry" in args and args["--retry"] is not None: retry = int(args["--retry"])
-	if args["add"] == True:
+	if args["add"] is True:
 		bumpversion = args["--bumpversion"]
 		run_fsck = args["--fsck"]
 		del_files = args["--del"]
 		r.add(spec, bumpversion, run_fsck, del_files)
-	if args["commit"] == True:
+	if args["commit"] is True:
 		dataset_tag = args["--dataset"]
 		labels_tag = args["--labels"]
 		tags = {}
@@ -72,64 +72,75 @@ def repository_entity_cmd(config, args):
 		if labels_tag is not None: tags["labels"] = labels_tag
 		run_fsck = args["--fsck"]
 		r.commit(spec, tags, run_fsck)
-	if args["push"] == True:
+	if args["push"] is True:
 		r.push(spec, retry)
-	if args["branch"] == True:
+	if args["branch"] is True:
 		r.branch(spec)
-	if args["status"] == True:
+	if args["status"] is True:
 		r.status(spec)
-	if args["show"] == True:
+	if args["show"] is True:
 		r.show(spec)
-	if args["tag"] == True:
+	if args["tag"] is True:
 		tag = args["<tag>"]
-		if args["add"] == True:
+		if args["add"] is True:
 			r.tag(spec, tag)
-		if args["list"] == True:
+		if args["list"] is True:
 			r.list_tag(spec)
 		return
 
 	tag = args["<ml-entity-tag>"]
-	if args["checkout"] == True:
+	if args["checkout"] is True:
 		r.checkout(tag)
-	if args["get"] == True:
+	if args["get"] is True:
+		samples = {}
 		if args['--group-sample']:
 			group_sample = args['--group-sample']
 			seed = args['--seed']
-			samples = {}
-			if group_sample is not None: samples["group"] = group_sample
-			if seed is not None: samples["seed"] = seed
+			if group_sample is not None:
+				samples["group"] = group_sample
+			if seed is not None:
+				samples["seed"] = seed
 			r.get(tag, samples, retry)
 		elif args['--range-sample']:
 			range_sample = args['--range-sample']
-			samples = {}
-			if range_sample is not None: samples["range"] = range_sample
+			if range_sample is not None:
+				samples["range"] = range_sample
+			r.get(tag, samples, retry)
+		elif args['--random-sample']:
+			random_sample = args['--random-sample']
+			if random_sample is not None:
+				samples["random"] = random_sample
 			r.get(tag, samples, retry)
 		else:
 			r.get(tag, None, retry)
-	if args["fetch"] == True:
+	if args["fetch"] is True:
+		samples = {}
 		if args['--group-sample']:
 			group_sample = args['--group-sample']
 			seed = args['--seed']
-			samples = {}
 			if group_sample is not None: samples["group"] = group_sample
 			if seed is not None: samples["seed"] = seed
 			r.get(tag, samples, retry)
 		elif args['--range-sample']:
 			range_sample = args['--range-sample']
-			samples = {}
 			if range_sample is not None: samples["range"] = range_sample
+			r.fetch(tag, samples)
+		elif args['--random-sample']:
+			random_sample = args['--random-sample']
+			if random_sample is not None:
+				samples["random"] = random_sample
 			r.fetch(tag, samples)
 		else:
 			r.fetch(tag, None)
-	if args["init"] == True:
+	if args["init"] is True:
 		r.init()
-	if args["update"] == True:
+	if args["update"] is True:
 		r.update()
-	if args["fsck"] == True:
+	if args["fsck"] is True:
 		r.fsck()
-	if args["gc"] == True:
+	if args["gc"] is True:
 		r.gc()
-	if args["list"] == True:
+	if args["list"] is True:
 		# TODO: use MetadataManager list in repository!
 		r.list()
 
@@ -144,8 +155,8 @@ def run_main():
 	ml-git (dataset|labels|model) (branch|show|status) <ml-entity-name> [--verbose]
 	ml-git (dataset|labels|model) push <ml-entity-name> [--retry=<retries>] [--verbose]
 	ml-git (dataset|labels|model) checkout <ml-entity-tag> [--verbose]
-	ml-git (dataset|labels|model) get <ml-entity-tag> [(--group-sample=<amount:group-size> --seed=<value> | --range-sample=<start:stop:step> )] [--retry=<retries>] [--verbose]
-	ml-git (dataset|labels|model) fetch <ml-entity-tag> [(--group-sample=<amount:group-size> --seed=<value> | --range-sample=<start:stop:step> )] [--verbose]
+	ml-git (dataset|labels|model) get <ml-entity-tag> [(--group-sample=<amount:group-size> --seed=<value> | --range-sample=<start:stop:step> | --random-sample=<amount:frequency>)] [--retry=<retries>] [--verbose]
+	ml-git (dataset|labels|model) fetch <ml-entity-tag> [(--group-sample=<amount:group-size> --seed=<value> | --range-sample=<start:stop:step> | --random-sample=<amount:frequency>)] [--verbose]
 	ml-git (dataset|labels|model) add <ml-entity-name> [--fsck] [--bumpversion] [--verbose] [--del]
 	ml-git dataset commit <ml-entity-name> [--tag=<tag>] [--verbose] [--fsck]
 	ml-git labels commit <ml-entity-name> [--dataset=<dataset-name>] [--tag=<tag>] [--verbose]
@@ -156,18 +167,19 @@ def run_main():
 
 	Options:
 	--credentials=<profile>            Profile of AWS credentials [default: default].
-	--fsck                             Run fsck after command execution
-	--del                              Persist the files' removal
+	--fsck                             Run fsck after command execution.
+	--del                              Persist the files' removal.
 	--region=<region>                  AWS region name [default: us-east-1].
 	--type=<store-type>                Data store type [default: s3h].
 	--tag                              A ml-git tag to identify a specific version of a ML entity.
-	--verbose                          Verbose mode
+	--verbose                          Verbose mode.
 	--bumpversion                      (dataset add only) increment the dataset version number when adding more files.
-	--retry=<retries>                  Number of retries to upload or download the files from the storage [default: 2]
+	--retry=<retries>                  Number of retries to upload or download the files from the storage [default: 2].
 	--group-sample=<amount:group-size> The group sample option consists of amount and group used to download a sample.
-	--seed=<value>				       The seed is used to initialize the pseudorandom numbers.
+	--seed=<value>                     The seed is used to initialize the pseudorandom numbers.
 	--range-sample=<start:stop:step>   The range sample option consists of start,stop and step and used to download
-	                                   a sample.The stop parameter can be all or -1 or any integer above zero
+	                                   a sample.The stop parameter can be all or -1 or any integer above zero.
+	--random-sample=<amount:frequency> The random sample option consists of amount and frequency and used to download a sample.
 	-h --help                          Show this screen.
 	--version                          Show version.
 	"""
--- src/mlgit/sample.py
+++ src/mlgit/sample.py
@@ -13,7 +13,7 @@ class SampleValidateExcepetion(Exception):
         super().__init__(msg)
 
 
-class GroupSample:
+class GroupSample(object):
 
     def __init__(self, amount, group_size, seed):
         self.__amount = amount
@@ -30,7 +30,7 @@ class GroupSample:
         return self.__seed
 
 
-class RangeSample:
+class RangeSample(object):
 
     def __init__(self, start, stop, step):
         self.__start = start
@@ -45,6 +45,18 @@ class RangeSample:
 
     def get_step(self):
         return self.__step
+
+
+class RandomSample(object):
+    def __init__(self, amount, frequency):
+        self.__amount = amount
+        self.__frequency = frequency
+
+    def get_amount(self):
+        return self.__amount
+
+    def get_frequency(self):
+        return self.__frequency
 
 
 class SampleValidate:
@@ -62,16 +74,20 @@ class SampleValidate:
             elif step < 0:
                 raise SampleValidateExcepetion("The step parameter above zero.")
             elif step > stop:
-                raise SampleValidateExcepetion("The stop parameter must be greater that of the step.")
+                raise SampleValidateExcepetion("The step parameter must be greater that of the stop.")
             elif stop > files_size:
                 raise SampleValidateExcepetion(
                     "The stop parameter must be smaller that of the file list.")
             elif step > files_size:
                 raise SampleValidateExcepetion(
                     "The step parameter must be smaller that of the file list.")
+            elif files_size is None or files_size == 0:
+                raise SampleValidateExcepetion(
+                    "The file list is empty.")
         else:
             raise SampleValidateExcepetion(
-                "The --range-sample=<start:stop:step> or  --range-sample=<start:stop>: requires integer values.The stop parameter can be all or -1 or any integer above zero")
+                "The --range-sample=<start:stop:step> or  --range-sample=<start:stop>:"
+                " requires integer values.The stop parameter can be all or -1 or any integer above zero")
         return RangeSample(start=start, stop=stop, step=step)
 
     @staticmethod
@@ -92,17 +108,44 @@ class SampleValidate:
             elif amount > files_size:
                 raise SampleValidateExcepetion(
                     "The amount must be smaller than that of the file list.")
+            elif files_size is None or files_size == 0:
+                raise SampleValidateExcepetion(
+                    "The file list is empty.")
         else:
             raise SampleValidateExcepetion(
                 "The --group-sample=<amount:group-size> --seed=<seed>: requires integer values.")
         return GroupSample(amount=amount, group_size=group_size, seed=seed)
 
     @staticmethod
+    def __random_sample_validation(sample, files_size):
+        re_sample = re.search(r"^(\d+)\:(\d+)$", sample)
+        if re_sample is not None:
+            amount = int(re_sample.group(1))
+            frequency = int(re_sample.group(2))
+            if frequency < 0:
+                raise SampleValidateExcepetion("The frequency  parameter above zero.")
+            elif amount >= frequency:
+                raise SampleValidateExcepetion("The amount parameter must be greater than that of the frequency.")
+            elif frequency > files_size:
+                raise SampleValidateExcepetion(
+                    "The frequency  parameter must be smaller than that of the file list.")
+            elif amount > files_size:
+                raise SampleValidateExcepetion(
+                    "The amount must be smaller than that of the file list.")
+            elif files_size is None or files_size == 0:
+                raise SampleValidateExcepetion(
+                    "The file list is empty.")
+        else:
+            raise SampleValidateExcepetion(
+                "The --random-sample=<amount:frequency> : requires integer values.")
+        return RandomSample(amount=amount, frequency=frequency)
+
+    @staticmethod
     def __stop_validate(stop, files_size):
-        if stop is 'all' or '-1':
+        if 'all' and '-1' in stop:
             return files_size
         else:
-            return stop
+            return int(stop)
 
     @staticmethod
     def __range_sample(start, stop, files, step, set_files):
@@ -127,6 +170,12 @@ class SampleValidate:
             SampleValidate.__group_sample(amount, div, files, parts, set_files, seed)
 
     @staticmethod
+    def __random_sample(amount, frequency, files, set_files):
+        for key in random.sample(range(len(files)), round((amount*len(files)/frequency))):
+            list_file = list(files)
+            set_files.update({list_file[key]: files.get(list_file[key])})
+
+    @staticmethod
     def process_samples(samples, files):
         set_files = {}
         try:
@@ -134,8 +183,8 @@ class SampleValidate:
                 if 'group' in samples:
                     group = SampleValidate.__group_sample_validation(samples['group'], samples['seed'], len(files))
                     if group:
-                        SampleValidate.__group_sample(group.get_amount(), group.get_group_size(), files, group.get_group_size(),
-                                     set_files, group.get_seed())
+                        SampleValidate.__group_sample(group.get_amount(), group.get_group_size(), files,
+                                                      group.get_group_size(), set_files, group.get_seed())
                         return set_files
                     else:
                         return None
@@ -143,7 +192,15 @@ class SampleValidate:
                     range_samp = SampleValidate.__range_sample_validation(samples['range'], len(files))
                     if range_samp:
                         SampleValidate.__range_sample(range_samp.get_start(), range_samp.get_stop(), files,
-                                     range_samp.get_step(), set_files)
+                                                      range_samp.get_step(), set_files)
+                        return set_files
+                    else:
+                        return None
+                elif 'random' in samples:
+                    random_samp = SampleValidate.__random_sample_validation(samples['random'], len(files))
+                    if random_samp:
+                        SampleValidate.__random_sample(random_samp.get_amount(), random_samp.get_frequency(), files,
+                                                       set_files)
                         return set_files
                     else:
                         return None
