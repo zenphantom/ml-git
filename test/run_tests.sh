d21ee6f9f0d4207465afedfd1eb629cb602669db
local <local@example.com>
2019-09-20 11:29:33 -0300

    Added local git server to clone test

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,4 +5,32 @@
 # SPDX-License-Identifier: GPL-2.0-only
 ################################################################################
 
+GIT=git_local_server.git
+
+mkdir -p $GIT
+
+git init --bare $GIT
+git clone $GIT/ master
+
+mkdir -p master/.ml-git
+
+echo "
+dataset:
+  git: https://git@github.com/standel/ml-datasets.git
+store:
+  s3:
+    mlgit-datasets:
+      aws-credentials:
+        profile: mlgit
+      region: us-east-1
+" > master/.ml-git/config.yaml
+
+git -C master add .
+git -C master commit -m "README.md"
+git -C master push origin master
+
+rm -rf master
+
 pytest --trace --cov-report term-missing --cov-report html:coverage --cov=mlgit --rootdir=../src
+
+rm -rf $GIT
