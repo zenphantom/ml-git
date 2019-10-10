dadb571120a0b9c1e75e7cc785167fe699edab96
Raiff Ramalho de Mello <raiff.mello@ccc.ufcg.edu.br>
2019-10-10 16:42:31 -0300

    adding permition to remove files from workspace

--- src/mlgit/workspace.py
+++ src/mlgit/workspace.py
@@ -5,6 +5,8 @@ SPDX-License-Identifier: GPL-2.0-only
 
 import os
 
+from mlgit.utils import set_write_read
+
 
 def remove_from_workspace(manifest_files, path, spec_name):
     for r, d, f in os.walk(path):
@@ -16,4 +18,6 @@ def remove_from_workspace(manifest_files, path, spec_name):
             for key in manifest_files:
                 for manifest_file in manifest_files[key]:
                     if file in manifest_file:
-                        os.unlink(os.path.join(path, file))
+                        filepath = os.path.join(path, file)
+                        set_write_read(filepath)
+                        os.unlink(filepath)
