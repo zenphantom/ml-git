efda7fef6f21771a4b98ac64aa86b9c8815c4d59
Abraão Santos <abraao.santos@virtus.ufcg.edu.br>
2019-10-25 11:44:34 -0300

    Fixing messages of checkout range-sample

--- src/mlgit/sample.py
+++ src/mlgit/sample.py
@@ -84,7 +84,7 @@ class SampleValidate:
         else:
             raise SampleValidateException(
                 "The --range-sample=<start:stop:step> or  --range-sample=<start:stop>:"
-                " requires integer values. The stop parameter can be 'all', '-1' or any integer greater than zero")
+                " requires positive integer values. The stop parameter can be 'all', '-1' or any integer greater than zero")
         return RangeSample(start=start, stop=stop, step=step)
 
     @staticmethod
@@ -107,7 +107,7 @@ class SampleValidate:
                     "The group size parameter should be smaller than the file list size.")
         else:
             raise SampleValidateException(
-                "The --group-sample=<amount:group-size> --seed=<seed>: requires integer values.")
+                "The --group-sample=<amount:group-size> --seed=<seed>: requires positive integer values.")
         return GroupSample(amount=amount, group_size=group_size, seed=seed)
 
     @staticmethod
@@ -131,7 +131,7 @@ class SampleValidate:
                     "The frequency  parameter should be smaller than the file list size.")
         else:
             raise SampleValidateException(
-                "The --random-sample=<amount:frequency> --seed=<seed>: requires integer values.")
+                "The --random-sample=<amount:frequency> --seed=<seed>: requires positive integer values.")
         return RandomSample(amount=amount, frequency=frequency, seed=seed)
 
     @staticmethod
@@ -206,8 +206,8 @@ class SampleValidate:
         if re.search(r"^(\d+)\:(all|-1|\d+)$", sample) is not None:
             range_regex = re.search(r"^(\d+)\:(all|-1|\d+)$", sample)
             return int(range_regex.group(1)), SampleValidate.__stop_validate(range_regex.group(2), files_size), 1
-        elif re.search(r"(\d+)\:(all|-1|\d+)\:(\d+)$", sample) is not None:
-            range_regex = re.search(r"(\d+)\:(all|-1|\d+)\:(\d+)$", sample)
+        elif re.search(r"^(\d+)\:(all|-1|\d+)\:(\d+)$", sample) is not None:
+            range_regex = re.search(r"^(\d+)\:(all|-1|\d+)\:(\d+)$", sample)
             return int(range_regex.group(1)), SampleValidate.__stop_validate(range_regex.group(2), files_size), int(
                 range_regex.group(3))
         else:
