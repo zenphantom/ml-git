9381db2029240d579ff59235202a3571685d23ea
Abraão Santos <abraao.santos@virtus.ufcg.edu.br>
2019-10-29 10:31:21 -0300

    Updating version

--- src/mlgit/main.py
+++ src/mlgit/main.py
@@ -242,7 +242,7 @@ def run_main():
 	config = config_load()
 	init_logger()
 
-	arguments = docopt(run_main.__doc__, version="0.8.4.1")
+	arguments = docopt(run_main.__doc__, version="0.9.12.1_RC")
 
 	main_validate(arguments)
 
