b7979b515ba59f91604a9090997b34cfc61413c1
Heitor <heitorcarrazzoni@gmail.com>
2019-11-20 15:45:07 -0300

    Fix add parameters

--- src/mlgit/main.py
+++ src/mlgit/main.py
@@ -206,7 +206,7 @@ def run_main():
 	ml-git (dataset|labels|model) tag <ml-entity-name> (add|del) <tag> [--verbose]
 	ml-git (dataset|labels|model) reset <ml-entity-name> (--hard|--mixed|--soft) (HEAD|HEAD~1) [--verbose]
 	ml-git config list
-	ml-git (dataset|labels|model) create <artefact-name> --category=<category-name>...  [<bucket-name>] [<store-type>] --version-number=<version-number> --import=<folder-name> [--wizzard-config] [--verbose]
+	ml-git (dataset|labels|model) create <artefact-name> --category=<category-name>...  [<store-type>] [<bucket-name>]  --version-number=<version-number> --import=<folder-name> [--wizzard-config] [--verbose]
 	ml-git (dataset|labels|model) import [--credentials=<profile>] [--region=<region-name>] [--retry=<retries>] [--path=<pathname>|--object=<object-name>] <bucket-name> <entity-dir> [--verbose]
 	ml-git clone <repository-url>
 	ml-git --version
