d669b80bd2e075d38772faa1f5f23be672df4822
Abraão Santos <abraao.santos@virtus.ufcg.edu.br>
2020-02-13 13:49:14 -0300

    Updating version to 1.3.9.1

--- src/mlgit/main.py
+++ src/mlgit/main.py
@@ -257,7 +257,7 @@ def run_main():
 	config = config_load()
 	init_logger()
 
-	arguments = docopt(run_main.__doc__, version="1.0.0.0")
+	arguments = docopt(run_main.__doc__, version="1.3.9.1")
 
 	main_validate(arguments)
 
