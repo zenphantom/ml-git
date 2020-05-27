26eebc43779b83c680c2fcff3413ca03345e5aff
Gilmar Bezerra de Freitas <gilmar.freitas@virtus.ufcg.edu.br>
2020-05-27 16:38:07 -0300

    #78875 - Double quotes changed to single quotes

--- tests/unit/test_refs.py
+++ tests/unit/test_refs.py
@@ -13,48 +13,48 @@ from mlgit.refs import Refs
 from mlgit.utils import yaml_load
 
 
-@pytest.mark.usefixtures("tmp_dir")
+@pytest.mark.usefixtures('tmp_dir')
 class RefsTestCases(unittest.TestCase):
 
     def test_init_refs(self):
         config = config_load()
-        spec_path = "dataset-ex"
-        ml_dir = os.path.join(self.tmp_dir, config["mlgit_path"])
+        spec_path = 'dataset-ex'
+        ml_dir = os.path.join(self.tmp_dir, config['mlgit_path'])
         os.mkdir(ml_dir)
-        refs_dir = os.path.join(ml_dir, "dataset", "refs")
-        refs = Refs(refs_dir, spec_path, "dataset")
+        refs_dir = os.path.join(ml_dir, 'dataset', 'refs')
+        refs = Refs(refs_dir, spec_path, 'dataset')
         self.assertIsNotNone(refs)
         self.assertTrue(os.path.exists(os.path.join(refs_dir, spec_path)))
 
     def test_update_head(self):
         config = config_load()
-        spec_path = "dataset-ex"
-        ml_dir = os.path.join(self.tmp_dir, config["mlgit_path"])
+        spec_path = 'dataset-ex'
+        ml_dir = os.path.join(self.tmp_dir, config['mlgit_path'])
         os.mkdir(ml_dir)
-        refs_dir = os.path.join(ml_dir, "dataset", "refs")
+        refs_dir = os.path.join(ml_dir, 'dataset', 'refs')
         refs = Refs(refs_dir, spec_path)
-        sha = "b569b7e4cd82206b451315123669057ef5f1ac3b"
-        tag = "images__dataset_ex__1"
+        sha = 'b569b7e4cd82206b451315123669057ef5f1ac3b'
+        tag = 'images__dataset_ex__1'
         refs.update_head(tag, sha)
-        head = os.path.join(refs_dir, spec_path, "HEAD")
+        head = os.path.join(refs_dir, spec_path, 'HEAD')
         self.assertTrue(os.path.exists(head))
         yaml = yaml_load(head)
         self.assertEqual(yaml[tag], sha)
 
     def test_head(self):
         config = config_load()
-        spec_path = "dataset-ex"
-        ml_dir = os.path.join(self.tmp_dir, config["mlgit_path"])
+        spec_path = 'dataset-ex'
+        ml_dir = os.path.join(self.tmp_dir, config['mlgit_path'])
         os.mkdir(ml_dir)
-        refs_dir = os.path.join(ml_dir, "dataset", "refs")
+        refs_dir = os.path.join(ml_dir, 'dataset', 'refs')
         refs = Refs(refs_dir, spec_path)
-        sha = "b569b7e4cd82206b451315123669057ef5f1ac3b"
-        tag = "images__dataset_ex__1"
+        sha = 'b569b7e4cd82206b451315123669057ef5f1ac3b'
+        tag = 'images__dataset_ex__1'
         refs.update_head(tag, sha)
-        head = os.path.join(refs_dir, spec_path, "HEAD")
+        head = os.path.join(refs_dir, spec_path, 'HEAD')
         self.assertEqual((tag, sha), refs.head())
         os.remove(head)
         self.assertEqual((None, None), refs.head())
 
-    if __name__ == "__main__":
+    if __name__ == '__main__':
         unittest.main()
--- tests/unit/test_workspace.py
+++ tests/unit/test_workspace.py
@@ -11,23 +11,23 @@ import pytest
 from mlgit.workspace import remove_from_workspace
 
 
-@pytest.mark.usefixtures("tmp_dir")
+@pytest.mark.usefixtures('tmp_dir')
 class RefsTestCases(unittest.TestCase):
 
     def create_file(self, directory, file_name):
         file = os.path.join(directory, file_name)
 
-        with open(file, "w"):
+        with open(file, 'w'):
             pass
 
         self.assertTrue(os.path.exists(file))
 
     def test_remove_from_workspace(self):
-        img = "image.jpg"
+        img = 'image.jpg'
         self.create_file(self.tmp_dir, img)
-        remove_from_workspace({img}, self.tmp_dir, "dataex")
+        remove_from_workspace({img}, self.tmp_dir, 'dataex')
         self.assertFalse(os.path.exists(os.path.join(self.tmp_dir, img)))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
