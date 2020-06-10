209749f9ebcb32167af1eb03789bc10093befd60
Gilmar Bezerra de Freitas <gilmar.freitas@virtus.ufcg.edu.br>
2020-06-10 18:09:17 -0300

    #79305 - Added integration test to import with gdrive

--- integration_test/test_22_import.py
+++ integration_test/test_22_import.py
@@ -7,7 +7,7 @@ import os
 import unittest
 
 from integration_test.commands import *
-from integration_test.helper import PATH_TEST, ML_GIT_DIR, entity_init
+from integration_test.helper import PATH_TEST, ML_GIT_DIR, entity_init, CREDENTIALS_PATH
 from integration_test.helper import check_output, clear, init_repository, add_file
 from integration_test.output_messages import messages
 
@@ -37,3 +37,20 @@ class ImportAcceptanceTests(unittest.TestCase):
 
         self.assertIn(messages[52] % "anyone", check_output(MLGIT_IMPORT % ("dataset", "bucket", "dataset-ex")
                                                             + " --credentials=anyone"))
+
+    def test_04_import_with_gdrive(self):
+        clear(ML_GIT_DIR)
+        init_repository("dataset", self)
+
+        file_path_b = os.path.join("dataset-ex", "B")
+        check_output(MLGIT_IMPORT % ("dataset", "mlgit", "dataset-ex")
+                     + "--store-type=gdrive --object=B --credentials="+CREDENTIALS_PATH)
+
+        self.assertTrue(os.path.exists(file_path_b))
+
+        file_path = os.path.join("dataset-ex", "test-folder", "A")
+
+        check_output(MLGIT_IMPORT % ("dataset", "mlgit", "dataset-ex")
+                     + "--store-type=gdrive --path=test-folder --credentials=" + CREDENTIALS_PATH)
+
+        self.assertTrue(os.path.exists(file_path))
