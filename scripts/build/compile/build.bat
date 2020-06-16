1593433b086efce4944b7ca4b517b5b810855ce3
Abraao Santos <abraao.santos@hp.com>
2020-06-16 12:25:21 -0300

    Updating build.bat to clean virtualenv before proceed and to not depend on ml-git application installation

--- scripts/build/compile/build.bat
+++ scripts/build/compile/build.bat
@@ -3,12 +3,13 @@
 :: SPDX-License-Identifier: GPL-2.0-only
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+pipenv uninstall --all
 pipenv install --ignore-pipfile --dev
 pipenv run python -O -m PyInstaller -D -n ml-git ./ml_git/main.py
 
 COPY .\scripts\build\compile\windows\install.ps1 dist\ml-git
 COPY .\scripts\build\compile\windows\uninstall.ps1 dist\ml-git
 
-for /f "tokens=*" %%i in ('pipenv run ml-git --version') do set build_name=%%i
+for /f "tokens=*" %%i in ('pipenv run python -c "import ml_git; print(ml_git.__version__)"') do set build_name=%%i
 
 tar -cvzf ".\dist\%build_name%_Windows.tar.gz" -C .\dist ml-git
