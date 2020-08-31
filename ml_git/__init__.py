c671b31b3801feac0af8a5afe86b0fa65c3e9fc8
Anderson Silva <anderson.silva@virtus.ufcg.edu.br>
2020-08-31 01:47:14 -0300

    Updating Github Actions configuration

--- ml_git/__init__.py
+++ ml_git/__init__.py
@@ -3,4 +3,3 @@
 SPDX-License-Identifier: GPL-2.0-only
 """
 
-__version__='0.0.1'
--- scripts/build/compile/build.sh
+++ scripts/build/compile/build.sh
@@ -14,6 +14,14 @@ pipenv run python -O -m PyInstaller -D -n ml-git ./ml_git/main.py
 
 cp ./scripts/build/compile/linux/install.sh ./scripts/build/compile/linux/uninstall.sh dist/ml-git/
 
-build_name=$(pipenv run python -c "import ml_git; print(ml_git.__version__)")
+BUILD_NUMBER=$(date +%Y%m%d)
+echo BUILD_NUMBER=$BUILD_NUMBER>build/version.info
 
-tar -cvzf "./dist/ml_git_${build_name}_Linux.tar.gz" -C dist ml-git
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+MAJOR_VERSION=$(cat "$PROJECT_ROOT/version.info" | grep MAJOR_VERSION | cut -d"=" -f2)
+MINOR_VERSION=$(cat "$PROJECT_ROOT/version.info" | grep MINOR_VERSION | cut -d"=" -f2)
+PATCH_VERSION=$(cat "$PROJECT_ROOT/version.info" | grep PATCH_VERSION | cut -d"=" -f2)
+
+BUILD_NAME="${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}-$BUILD_NUMBER"
+
+tar -cvzf "./dist/ml_git_${BUILD_NAME}_Linux.tar.gz" -C dist ml-git
