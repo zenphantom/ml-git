ae2a3d66748a7bf6695cf77f4c0efc53a2339b2e
Anderson Pablo <andersonpablo@gmail.com>
2020-10-20 15:13:12 -0300

    Update command to warn the user if a remote is already set

--- ml_git/admin.py
+++ ml_git/admin.py
@@ -10,6 +10,7 @@ from git import Repo, GitCommandError
 from ml_git import log
 from ml_git.config import mlgit_config_save, get_global_config_path
 from ml_git.constants import ROOT_FILE_NAME, CONFIG_FILE, ADMIN_CLASS_NAME, StoreType
+from ml_git.ml_git_message import output_messages
 from ml_git.storages.store_utils import get_bucket_region
 from ml_git.utils import get_root_path
 from ml_git.utils import yaml_load, yaml_save, RootPathException, clear, ensure_path_exists
@@ -52,12 +53,13 @@ def remote_add(repotype, ml_git_remote, global_conf=False):
 
     if repotype in conf:
         if conf[repotype]['git'] is None or not len(conf[repotype]['git']) > 0:
-            log.info('Add remote repository [%s] for [%s]' % (ml_git_remote, repotype), class_name=ADMIN_CLASS_NAME)
+            log.info(output_messages['INFO_ADD_REMOTE'] % (ml_git_remote, repotype), class_name=ADMIN_CLASS_NAME)
         else:
-            log.info('Changing remote from [%s]  to [%s] for  [%s]' % (conf[repotype]['git'], ml_git_remote, repotype),
+            log.warn(output_messages['WARN_HAS_CONFIGURED_REMOTE'], class_name=ADMIN_CLASS_NAME)
+            log.info(output_messages['INFO_CHANGING_REMOTE'] % (conf[repotype]['git'], ml_git_remote, repotype),
                      class_name=ADMIN_CLASS_NAME)
     else:
-        log.info('Add remote repository [%s] for [%s]' % (ml_git_remote, repotype), class_name=ADMIN_CLASS_NAME)
+        log.info(output_messages['INFO_ADD_REMOTE'] % (ml_git_remote, repotype), class_name=ADMIN_CLASS_NAME)
     try:
         conf[repotype]['git'] = ml_git_remote
     except Exception:
--- ml_git/ml_git_message.py
+++ ml_git/ml_git_message.py
@@ -4,13 +4,18 @@ SPDX-License-Identifier: GPL-2.0-only
 """
 
 output_messages = {
+    'INFO_INITIALIZED_PROJECT': 'Initialized empty ml-git repository in %s',
+    'INFO_ADD_REMOTE': 'Add remote repository [%s] for [%s]',
     'INFO_CHECKOUT_LATEST_TAG': 'Performing checkout on the entity\'s lastest tag (%s)',
     'INFO_CHECKOUT_TAG': 'Performing checkout in tag %s',
     'INFO_METADATA_INIT': 'Metadata init [%s] @ [%s]',
     'INFO_COMMIT_REPO': 'Commit repo[%s] --- file[%s]',
+    'INFO_CHANGING_REMOTE': 'Changing remote from [%s] to [%s] for [%s]',
 
     'ERROR_WITHOUT_TAG_FOR_THIS_ENTITY': 'No entity with that name was found.',
     'ERROR_MULTIPLES_ENTITIES_WITH_SAME_NAME': 'You have more than one entity with the same name. Use one of the following tags to perform the checkout:\n',
     'ERROR_WRONG_VERSION_NUMBER_TO_CHECKOUT': 'The version specified for that entity does not exist. Last entity tag:\n\t%s',
-    'ERROR_UNINITIALIZED_METADATA': 'You don\'t have any metadata initialized'
+    'ERROR_UNINITIALIZED_METADATA': 'You don\'t have any metadata initialized',
+
+    'WARN_HAS_CONFIGURED_REMOTE': 'YOU ALREADY HAS A CONFIGURED REMOTE. All data stored in this repository will be sent to the new one on the first push.',
 }
