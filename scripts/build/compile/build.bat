cc39621b4ffa713d37dda3782ab262631385441b
Lucas <lucas.cabral@virtus.ufcg.edu.br>
2020-11-19 16:15:39 -0300

    Moving dependencies to pipfile

--- scripts/build/compile/build.bat
+++ scripts/build/compile/build.bat
@@ -4,7 +4,7 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 pipenv uninstall --all
-pipenv install --ignore-pipfile --dev
+pipenv install --dev
 pipenv run python -O -m PyInstaller -D -n ml-git ./ml_git/main.py
 
 mkdir dist\ml-git
