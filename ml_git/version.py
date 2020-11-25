de34fc47020c1efbb9dec33530266a43402d3d70
gilMars <gilmar.lilium@gmail.com>
2020-11-25 17:15:06 -0300

    Changes because of build process

--- ml_git/version.py
+++ ml_git/version.py
@@ -12,9 +12,9 @@ def get_version():
     version_info = read_info_file(os.path.dirname(__file__), 'version.info')
     __version__ = '{}.{}.{}'.format(version_info['MAJOR_VERSION'], version_info['MINOR_VERSION'], version_info['PATCH_VERSION'])
 
-    build_number_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'build', 'version.info'))
-    if os.path.exists(build_number_file_path):
-        build_number_info = read_info_file(build_number_file_path)
+    build_number_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'build'))
+    if os.path.exists(os.path.join(build_number_file_path, 'version.info')):
+        build_number_info = read_info_file(build_number_file_path, 'version.info')
         __version__ += '-' + build_number_info['BUILD_NUMBER']
 
     return __version__
@@ -22,9 +22,9 @@ def get_version():
 
 def read_info_file(dir_name, file_name):
     try:
-        file = io.StringIO(resource_string(__name__, file_name).decode('utf-8'))
-    except FileNotFoundError:
         file = open(os.path.abspath(os.path.join(dir_name, file_name)), encoding='utf-8')
+    except NotADirectoryError:
+        file = io.StringIO(resource_string(__name__, file_name).decode('utf-8'))
 
     line = file.readline().strip()
     dict_info = {}
