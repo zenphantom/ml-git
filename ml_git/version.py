dd685ee4e41fd4862ac7f31cd09a1afec9580520
gilMars <gilmar.lilium@gmail.com>
2020-11-30 09:02:40 -0300

    Code adjusted according to review

--- ml_git/version.py
+++ ml_git/version.py
@@ -8,34 +8,21 @@ import os
 from pkg_resources import resource_string
 
 
-def get_version():
-    version_file_name = 'version.info'
-    version_info = get_version_info(version_file_name)
-    __version__ = '{}.{}.{}'.format(version_info['MAJOR_VERSION'], version_info['MINOR_VERSION'], version_info['PATCH_VERSION'])
-
-    build_number = get_build_number(version_file_name)
-
-    if build_number:
-        __version__ += '-' + build_number['BUILD_NUMBER']
-
-    return __version__
-
-
-def get_version_info(file_name):
+def _get_version_info(file_name):
     file_stream = io.StringIO(resource_string(__name__, file_name).decode('utf-8'))
-    return read_info_file(file_stream)
+    return _read_info_file(file_stream)
 
 
-def get_build_number(file_name):
+def _get_build_number(file_name):
     build_number_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'build'))
     build_version_info_path = os.path.join(build_number_file_path, file_name)
     if os.path.exists(build_version_info_path):
         file_stream = open(os.path.abspath(build_version_info_path), encoding='utf-8')
-        return read_info_file(file_stream)
+        return _read_info_file(file_stream)
     return None
 
 
-def read_info_file(file_stream):
+def _read_info_file(file_stream):
 
     line = file_stream.readline().strip()
     dict_info = {}
@@ -45,3 +32,16 @@ def read_info_file(file_stream):
         line = file_stream.readline().strip()
     file_stream.close()
     return dict_info
+
+
+def get_version():
+    version_file_name = 'version.info'
+    version_info = _get_version_info(version_file_name)
+    __version__ = '{}.{}.{}'.format(version_info['MAJOR_VERSION'], version_info['MINOR_VERSION'], version_info['PATCH_VERSION'])
+
+    build_number = _get_build_number(version_file_name)
+
+    if build_number:
+        __version__ += '-' + build_number['BUILD_NUMBER']
+
+    return __version__
