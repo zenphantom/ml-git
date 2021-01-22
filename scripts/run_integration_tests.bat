0b0cf4a149f43bb9a86e45ab43af0d4b21ae2755
Felipe Muniz <felipemuniznet@gmail.com>
2021-01-22 14:12:15 -0300

    Generation of fake_ssh_key on integrations tests execution.

--- scripts/run_integration_tests.bat
+++ scripts/run_integration_tests.bat
@@ -44,7 +44,9 @@ START docker run -p 10000:10000 --name azure ^
 -v "%PATH_TEST%\data:/data"  ^
 mcr.microsoft.com/azure-storage/azurite azurite-blob --blobHost 0.0.0.0
 
-START docker run --name=sftp -v "%INTEGRATION_TESTS_BASE_PATH%\fake_ssh_key\id_rsa.pub":/home/mlgit_user/.ssh/keys/id_rsa.pub:ro ^
+ssh-keygen -t rsa -N "" -b 4096 -f "%INTEGRATION_TESTS_BASE_PATH%\fake_ssh_key\test_key"
+
+START docker run --name=sftp -v "%INTEGRATION_TESTS_BASE_PATH%\fake_ssh_key\test_key.pub":/home/mlgit_user/.ssh/keys/test_key.pub:ro ^
 -v "%PATH_TEST%\sftp\mlgit":/home/mlgit_user/mlgit ^
 -p 9922:22 -d atmoz/sftp ^
 mlgit_user::1001:::mlgit
